e271033f81d7dfef8f917a8c6f03d6e085e3fc97
Chengyu Sun <91579975+sunchengyu1111@users.noreply.github.com>
2022-04-12 16:34:45 +0800

    Add files via upload

--- Figure/Typical_rowcoleffi.py
+++ Figure/Typical_rowcoleffi.py
@@ -12,9 +12,9 @@ def draw_line_chart():
 	y1 = [1336.668,1390.35,1424.808,1440.944,1460.896,1419.264,1436.288,1370.592,1366.016,1349.664]
 	for num in range (0,len(y1)):
 		y1[num]=1336.668/y1[num]
-	y2 = [10107.050,6061.797,3060.433,2156.773,1408.437,674.577,244.983,131.507,61.725,51.289]
+	y2 = [10107.050,12123.594,12241.732,17254.184,22534.992,21586.464,15678.912,16832.896,15801.6,26259.968]
 	for num in range (0,len(y2)):
-		y2[num]=10107.050/y2[num]/pow(2,num)
+		y2[num]=10107.050/y2[num]
 	plt.plot(x,y1,'*-',alpha=0.8,color='darkviolet',linewidth=1,label='Row rescaling')
 	plt.plot(x,y2,'x-',alpha=0.8,color='chocolate',linewidth=2,label='Column rescaling')
 	
