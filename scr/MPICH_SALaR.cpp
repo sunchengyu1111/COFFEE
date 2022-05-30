6e962c11c3569b4f138f919334b13d00af2230de
Chengyu Sun <91579975+sunchengyu1111@users.noreply.github.com>
2022-05-30 16:56:54 +0800

    Update and rename Source Code/MPICH_SALaR_distributed.cpp to scr/MPICH_SALaR.cpp

--- scr/MPICH_SALaR.cpp
+++ scr/MPICH_SALaR.cpp
@@ -96,7 +96,7 @@ int main(int argc,char *argv[]){
         MPI_Bcast(sumofrow,m*w,MPI_DOUBLE,0,MPI_COMM_WORLD);
         MPI_Bcast(sumofcol,n,MPI_DOUBLE,0,MPI_COMM_WORLD);
 
-        //start loop
+        //start
         int count=0;
         double error=0;
         bool loop=true;
@@ -166,7 +166,6 @@ int main(int argc,char *argv[]){
 				*/
 			}
 			
-			//do i reduce
 			if(MPI_COMM_NULL!=worker_comm){
                         	MPI_Reduce(local_sumofcol+i*each_iterlength,reduce_sumofcol,each_iterlength,MPI_DOUBLE,MPI_SUM,0,worker_comm);
                        		/* 		
@@ -180,7 +179,6 @@ int main(int argc,char *argv[]){
                         	*/
                 	}
 			
-			//do i-1 allreduce
 			if(MPI_COMM_NULL!=leader_comm){
 				for(int j=0;j<each_iterlength;j++){
 					allreduce_sumofcol[j]+=local_sumofcol[j+(i-1)*each_iterlength];
@@ -196,7 +194,6 @@ int main(int argc,char *argv[]){
                 	        */
                 	}
 			
-			//do i-1 bcast
 			MPI_Bcast(local_sumofcol+(i-1)*each_iterlength,each_iterlength,MPI_DOUBLE,local_size-1,local_comm);
 			/*
                         printf("rank:%d,here5,local_sumofcol\n",world_rank);
@@ -207,7 +204,6 @@ int main(int argc,char *argv[]){
                        	*/
 		}
 		
-		//do last allreduce
                 if(local_rank==0){
                         MPI_Send(reduce_sumofcol,each_iterlength,MPI_DOUBLE,local_size-1,0,local_comm);
                 }
@@ -226,7 +222,7 @@ int main(int argc,char *argv[]){
                         for(int j=0;j<each_iterlength;j++){
                                 allreduce_sumofcol[j]+=local_sumofcol[j+(total_iternum-1)*each_iterlength];
                         }
-			//usleep(110000);
+			//usleep(160000);
                         MPI_Allreduce(allreduce_sumofcol,local_sumofcol+(total_iternum-1)*each_iterlength,each_iterlength,MPI_DOUBLE,MPI_SUM,leader_comm);
 			/*                              
                         printf("rank:%d,here7,allreduce_sumofcol\n",world_rank);
@@ -237,7 +233,6 @@ int main(int argc,char *argv[]){
 			*/
                 }
 		
-		//do last bcast
 		MPI_Bcast(local_sumofcol+(total_iternum-1)*each_iterlength,each_iterlength,MPI_DOUBLE,local_size-1,local_comm);
 		/*		
 		printf("rank:%d,here8,local_sumofcol\n",world_rank);
@@ -247,7 +242,6 @@ int main(int argc,char *argv[]){
                 printf("\n");
 		*/
 		
-		//count col
                 for(int i=0;i<n;i++){
 			double temp=sumofcol[i]/local_sumofcol[i];
                         for(int j=0;j<m;j++){
@@ -255,7 +249,7 @@ int main(int argc,char *argv[]){
                         }
                 }
 
-                //calculate total error
+                //cal error
                 double temp1=0,temp2=0;
                 for(int i=0;i<m*n;i++){
                         temp2=fabs(local_sinkhorn[i]-copy_sinkhorn[i]);
@@ -270,7 +264,7 @@ int main(int argc,char *argv[]){
         MPI_Allreduce(&time01,&time1,1,MPI_DOUBLE,MPI_MIN,MPI_COMM_WORLD);
         if(world_rank==0) printf("%f, ",time1);
 
-	//end
+	//finish
         MPI_Finalize();
         delete []sumofrow;
         delete []sumofcol;
