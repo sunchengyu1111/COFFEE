61748df303ec5a10c5b28d6d0f07726675a23ac8
Chengyu Sun <91579975+sunchengyu1111@users.noreply.github.com>
2022-05-30 16:19:58 +0800

    Update and rename Source Code/typical_+C2R.cpp to src/typical_+C2R.cpp

--- src/typical_+C2R.cpp
+++ src/typical_+C2R.cpp
@@ -14,7 +14,6 @@ int main(int argc,char *argv[]){
 	MPI_Init(NULL,NULL);
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-	
     	double *local_sinkhorn=new double[m*n];
 	double *copy_sinkhorn=new double[m*n];
 	double *sumofcol=new double[n];
